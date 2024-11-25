318eeee0b0e7c5e3febb0b2d637a62166cc080d3
Sambath Chan <sambathc98@gmail.com>
2024-11-25 17:07:08 -0600

    fix layout empty posts

--- src/components/shared/Posts.tsx
+++ src/components/shared/Posts.tsx
@@ -1,5 +1,5 @@
-import { Post } from "./index";
 import { IPost } from "../../models";
+import { Post } from "./index";
 import sadIcon from "../../assets/sad.svg";
 
 type PostsProps = {
@@ -12,9 +12,9 @@ function Posts({ posts }: PostsProps) {
       {
         posts.length ?
           (posts.map((post) => (
-            <Post key={post.uuid} post={post} single={false} />
+            <Post key={post._id} post={post} single={false} />
           ))) :
-          <div className="text-center text-red-500">
+          <div className="p-2 sm:w-[600px] lg:w-[750px] text-center text-red-500">
             <h4 className="my-5">There are no posts to display</h4>
             <img className="mx-auto" width={50} src={sadIcon} alt="sad face" />
           </div>
